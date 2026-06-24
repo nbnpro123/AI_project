86f80fb597e2570931be8fe2420bd35ef16c90d0
nbnpro123 <prohorovict39@gmail.com>
2026-06-24 12:21:12 +0300

    resize img

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 
 
 img = cv2.imread(r"D:\Proekts\pythonProject\images\1.jpg")
-cv2.imshow('Result',img)
+img = cv2.resize(img,(img.shape[1] // 2, img.shape[0] // 2 ))
 
+cv2.imshow('Result',img)
+print(img.shape)
 cv2.waitKey(0)
