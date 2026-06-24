9cb09e1922b7daddb281afe82d0ff04d2c007a10
nbnpro123 <prohorovict39@gmail.com>
2026-06-24 12:29:40 +0300

    Preparing images for AI analysis

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import cv2
 
 img = cv2.imread(r"D:\Proekts\pythonProject\images\1.jpg")
 img = cv2.resize(img,(img.shape[1] // 2, img.shape[0] // 2 ))
+img = cv2.GaussianBlur(img,(5,5),0)
+img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+img = cv2.Canny(img, 90, 90)
 
 cv2.imshow('Result',img)
 print(img.shape)
