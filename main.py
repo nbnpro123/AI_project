d8a397be7736b51ac527b097a5f52c56b10502ad
nbnpro123 <prohorovict39@gmail.com>
2026-06-25 14:42:24 +0300

    added rotation images and bias images

--- main.py
+++ main.py
@@ -41,36 +41,59 @@ import numpy as np
 
 
 
-# Подготовка видео к анализу
-cap = cv2.VideoCapture(r"D:\Proekts\pythonProject\videos\video1.mp4")
+# # Подготовка видео к анализу
+# cap = cv2.VideoCapture(r"D:\Proekts\pythonProject\videos\video1.mp4")
+#
+# while True:
+#     success, img = cap.read()
+#     # img = cv2.resize(img,(img.shape[1] // 2, img.shape[0] // 2 ))
+#     img = cv2.GaussianBlur(img,(5,5),0)
+#     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#
+#     img = cv2.Canny(img, 100, 100)
+#
+#
+#
+#     kernel = np.ones((5,5),np.uint8)
+#
+#
+#     img = cv2.dilate(img, kernel, iterations=1)
+#
+#
+#     img = cv2.erode(img, kernel, iterations=1)
+#
+#     cv2.imshow('Result',img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+#
+#
+# cv2.waitKey(0)
 
-while True:
-    success, img = cap.read()
-    # img = cv2.resize(img,(img.shape[1] // 2, img.shape[0] // 2 ))
-    img = cv2.GaussianBlur(img,(5,5),0)
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    img = cv2.Canny(img, 100, 100)
 
 
-
-    kernel = np.ones((5,5),np.uint8)
-
-
-    img = cv2.dilate(img, kernel, iterations=1)
+img = cv2.imread(r"D:\Proekts\pythonProject\images\1.jpg")
+img = cv2.resize(img,(img.shape[1] // 2, img.shape[0] // 2 ))
+# img = cv2.flip(img, 1)
 
 
-    img = cv2.erode(img, kernel, iterations=1)
+def rotate(img_param, angle):
+    height, width = img_param.shape[:2]
+    point = (width // 2, height // 2)
 
-    cv2.imshow('Result',img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    mat = cv2.getRotationMatrix2D(point, angle, 1.0)
+    return cv2.warpAffine(img_param, mat, (width, height))
+# img = rotate(img, 90)
+
+def transform(img_param, x, y):
+    mat = np.float32([[1, 0, x], [0, 1, y]])
+    return cv2.warpAffine(img_param, mat, (img_param.shape[1], img_param.shape[0]))
+
+img = transform(img, 30, 200)
 
 
-
+cv2.imshow("Image", img)
 cv2.waitKey(0)
-
-
 
 
 
