20675e6a9f8a116de01f758a2e1018d986a118e0
nbnpro123 <prohorovict39@gmail.com>
2026-06-25 14:47:19 +0300

    added rotation images and bias images

--- main.py
+++ main.py
@@ -22,11 +22,9 @@ import numpy as np
 # cv2.imshow('Result',img)
 # print(img.shape)
 # cv2.waitKey(0)
-
-
-
-
-
+#
+#
+#
 # #Чёрное фото с фигурами
 #
 #
@@ -69,41 +67,40 @@ import numpy as np
 #
 #
 # cv2.waitKey(0)
+#
+#
+#
+# img = cv2.imread(r"D:\Proekts\pythonProject\images\1.jpg")
+# img = cv2.resize(img,(img.shape[1] // 2, img.shape[0] // 2 ))
+# # img = cv2.flip(img, 1)
+#
+#
+# def rotate(img_param, angle):
+#     height, width = img_param.shape[:2]
+#     point = (width // 2, height // 2)
+#
+#     mat = cv2.getRotationMatrix2D(point, angle, 1.0)
+#     return cv2.warpAffine(img_param, mat, (width, height))
+# # img = rotate(img, 90)
+#
+# def transform(img_param, x, y):
+#     mat = np.float32([[1, 0, x], [0, 1, y]])
+#     return cv2.warpAffine(img_param, mat, (img_param.shape[1], img_param.shape[0]))
+#
+# img = transform(img, 30, 200)
+#
+#
+# cv2.imshow("Image", img)
+# cv2.waitKey(0)
+
+
 
 
 
 img = cv2.imread(r"D:\Proekts\pythonProject\images\1.jpg")
-img = cv2.resize(img,(img.shape[1] // 2, img.shape[0] // 2 ))
-# img = cv2.flip(img, 1)
-
-
-def rotate(img_param, angle):
-    height, width = img_param.shape[:2]
-    point = (width // 2, height // 2)
-
-    mat = cv2.getRotationMatrix2D(point, angle, 1.0)
-    return cv2.warpAffine(img_param, mat, (width, height))
-# img = rotate(img, 90)
-
-def transform(img_param, x, y):
-    mat = np.float32([[1, 0, x], [0, 1, y]])
-    return cv2.warpAffine(img_param, mat, (img_param.shape[1], img_param.shape[0]))
-
-img = transform(img, 30, 200)
-
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
-
-
-
-
-
-
-
-
-
-
 
 
 
